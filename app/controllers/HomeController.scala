9f670f7ee53814fc26a4f2f4882523d6ddcc4dea
hobbist <kapil.21.kulkarni21@gmail.com>
2020-10-07 23:21:30 +0530

    views and images

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,18 +2,19 @@ package controllers
 
 import javax.inject.Inject
 import play.api.mvc._
+import play.api._
 
 import scala.concurrent._
 
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents) (implicit assetsFinder: AssetsFinder) extends BaseController {
 
   def action: Action[AnyContent] = Action { implicit request =>
-    val r: Result = Ok("hello world")
+    val r: Result =Ok(views.html.welcome("user"))
     r
   }
 
   def asyncIndex: Action[AnyContent] = Action.async { implicit request =>
-    val r: Future[Result] = Future.successful(Ok("hello world"))
+    val r: Future[Result] = Future.successful(Ok(views.html.welcome("user")))
     r
   }
 }
